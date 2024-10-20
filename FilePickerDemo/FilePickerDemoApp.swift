bbaa2bc8f9b1d1e5367b06bdadf6d8063698f2fe
Jędrzej Chołuj <jedrzej.choluj@coi.gov.pl>
2024-10-20 17:13:37 +0200

    Initial Commit

---
+++ FilePickerDemo/FilePickerDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilePickerDemoApp.swift
+//  FilePickerDemo
+//
+//  Created by Choluj Jedrzej (BL) on 20/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct FilePickerDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
